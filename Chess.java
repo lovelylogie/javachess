2a733f4daa3716d4c7ea425dfa5d12c6fcc32197
Scotty882 <63341889+Scotty882@users.noreply.github.com>
2021-08-10 15:26:22 +0800

    Add files via upload
    No longer requires the pawn class or piece colour class. Put the contents into the chess class to make the multi moves work. Started on castle but haven't figured out the best way yet

--- Chess.java
+++ Chess.java
@@ -9,12 +9,9 @@ public class Chess
     public int[] possibleMoves;
     private Pieces selectedPiece;
     
-    private King   king;
-    private Queen  queen;
-    private Bishop bishop;
-    private Knight knight;
-    private Castle castle;
-    private Pawn   pawn;
+    private ChessViewer screen;
+    
+
     
     private Pieces[][] board;
 
@@ -22,7 +19,6 @@ public class Chess
     {
         board = new Pieces[size][size];
         this.selectedPiece = null;
-        this.isPieceSelected = false;
         
         // kings
         
@@ -81,7 +77,6 @@ public class Chess
     public void movePiece(int x1, int y1, int x2, int y2) {
             board[x1][y1] = Pieces.EMPTY;
             board[x2][y2] = selectedPiece;
-            this.isPieceSelected = false;
             viewer.displayBoard();
     }
     
@@ -94,25 +89,78 @@ public class Chess
                 if (x == x_coordinate && y == y_coordinate) 
                     {movePiece(pieceSelectedCoordinate[0], pieceSelectedCoordinate[1], x, y);}
             }
-            if (getBoard(x, y) == Pieces.EMPTY) {this.isPieceSelected = false; viewer.displayBoard(); return;}
         }
-        else if (getBoard(x, y) != Pieces.EMPTY) {
-            this.selectedPiece = getBoard(x, y);
-            this.isPieceSelected = true;
-            this.pieceSelectedCoordinate[0] = x; 
-            this.pieceSelectedCoordinate[1] = y;
-            String piece = selectedPiece.name().substring(6); // turns the enum into a string so that the piece's class can be called
-            if (piece.equals("KING"))   {}                    // cuts it off at index 6 to get rid of BLACK_ or WHITE_ 
-            if (piece.equals("QUEEN"))  {}
-            if (piece.equals("BISHOP")) {}
-            if (piece.equals("KNIGHT")) {}
-            if (piece.equals("CASTLE")) {}
-            if (piece.equals("PAWN"))   {pawn = new Pawn(); pawn.possibleMoves(x, y);} 
-        }
-        else return;           
+        if (getBoard(x, y) == Pieces.EMPTY) {this.isPieceSelected = false; return;}
+        else                                {this.selectedPiece = getBoard(x, y);}
+        this.isPieceSelected = true;
+        this.pieceSelectedCoordinate[0] = x; 
+        this.pieceSelectedCoordinate[1] = y;
+        String piece = selectedPiece.name().substring(6); // turns the enum into a string so that the piece's class can be called
+        if (piece.equals("KING"))   {}                    // cuts it off at index 6 to get rid of BLACK_ or WHITE_ 
+        if (piece.equals("QUEEN"))  {}
+        if (piece.equals("BISHOP")) {}
+        if (piece.equals("KNIGHT")) {}
+        if (piece.equals("CASTLE")) {castle(x, y);}
+        if (piece.equals("PAWN"))   {pawn(x, y);}            
     }
     
     public Pieces getBoard(int x, int y) {
         return this.board[x][y];
     }
-}
+    
+    public void pawn(int x, int y)
+    {
+        // find out what colour the piece selected is
+        String possibleMoves = "";
+        // since pawns can only move in a forward direction, this gets a bit tricky
+        // if the pawn is white
+        if (board[x][y] == Pieces.valueOf("WHITE_PAWN")) {
+            if (y != 7) {
+            if (getBoard(x, y - 1) == Pieces.EMPTY)                            // up 
+                {possibleMoves += x;       possibleMoves += (y - 1) + " ";}
+            // if white pawn hasn't moved yet
+            if (y == 6 && getBoard(x, y - 1) == Pieces.EMPTY) {
+                // Stops pawn jumping over pieces
+                if (getBoard(x, y - 2) == Pieces.EMPTY)                            // up + 1 (start)
+                    {possibleMoves += x;       possibleMoves += (y - 2) + " ";}  } 
+            if (isLegal (x - 1, y - 1) && 
+                getBoard(x - 1, y - 1).name().substring(0,5).equals("BLACK"))  // up left
+                {possibleMoves += (x - 1); possibleMoves += (y - 1) + " ";}
+            if (isLegal (x + 1, y - 1) && 
+                getBoard(x + 1, y - 1).name().substring(0,5).equals("BLACK"))  // up right
+                {possibleMoves += (x + 1); possibleMoves += (y - 1) + " ";}
+        }
+    }
+        
+        // if the pawn is black
+        if (board[x][y] == Pieces.valueOf("BLACK_PAWN")) {
+            if (y != 0) {
+            if (getBoard(x, y + 1 ) == Pieces.EMPTY)                            // down 
+                {possibleMoves += x;       possibleMoves += (y + 1) + " ";}
+            // if black pawn hasn't moved yet
+            if (y == 1) {
+            // Stops pawn jumping over pieces
+            if (getBoard(x, y + 2) == Pieces.EMPTY && getBoard(x, y + 1) == Pieces.EMPTY)                           // down + 1 (start)
+                {possibleMoves += x;       possibleMoves += (y + 2) + " ";}  }  
+            if (isLegal (x - 1, y + 1) &&
+                getBoard(x - 1, y + 1).name().substring(0,5).equals("WHITE"))  // down left           
+                {possibleMoves += (x - 1); possibleMoves += (y + 1) + " ";}
+            if (isLegal (x + 1, y + 1) &&
+                getBoard(x + 1, y + 1).name().substring(0,5).equals("WHITE"))  // down right         
+                {possibleMoves += (x + 1); possibleMoves += (y + 1) + " ";}
+        }
+    }
+        //System.out.println(possibleMoves);
+        screen.drawPossibleMoves(possibleMoves);
+    }
+    public void castle (int x, int y){
+        String possibleMoves = "";
+        if (board[x][y] == Pieces.valueOf("BLACK_CASTLE")) {
+        for(int z = y; getBoard(x,z).name().substring(0,5) != "BLACK"; z++) {
+            {possibleMoves += x;       possibleMoves += (y + 1) + " ";}
+        }
+        }
+        screen.drawPossibleMoves(possibleMoves);
+    }
+    }
+
