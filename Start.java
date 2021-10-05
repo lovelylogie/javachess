f676f9194f6811230fdbaaad213ae89213c21ce3
intelliw <mchanmugam@gmail.com>
2021-10-05 14:24:38 +0800

    <Start>: add main

--- Start.java
+++ Start.java
@@ -8,15 +8,21 @@
 public class Start
 {
     // instance variables - replace the example below with your own
-    private Chess board;
-    private ChessViewer viewer;
+    //private Chess board;
+    //private ChessViewer viewer;
 
     /**
      * Constructor for objects of class start
      */
     public Start()
     {
-        board = new Chess();
-        viewer = new ChessViewer();
+        //board = new Chess();
+        //viewer = new ChessViewer();
     }
+    public static void main(String[] args) {
+        ChessViewer app = new ChessViewer();
+        System.out.println("Console is: " + System.console());
+    }
+    
+        
 }
